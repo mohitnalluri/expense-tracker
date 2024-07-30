abc9f9b735794f184a37c0e9d47eb07fc18ba115
N Mohit Sriram Narayana <115566663+mohitnalluri@users.noreply.github.com>
2024-07-30 19:54:30 +0530

    Updated Commit

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,4 @@
  const host = "https://expense-tracker-app-knl1.onrender.com";
-//const host = "https://exquisite-pavlova-ec673d.netlify.app"
 //const host = "http://localhost:5000";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
