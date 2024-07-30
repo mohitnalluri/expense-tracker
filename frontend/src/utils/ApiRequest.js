135798a01970df9d9d6905c8d949350ce7931faa
N Mohit Sriram Narayana <115566663+mohitnalluri@users.noreply.github.com>
2024-07-30 19:16:32 +0530

    Add files via upload

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
  const host = "https://expense-tracker-app-knl1.onrender.com";
-// const host = "https://exquisite-pavlova-ec673d.netlify.app"
+//const host = "https://exquisite-pavlova-ec673d.netlify.app"
 //const host = "http://localhost:5000";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
@@ -8,15 +7,4 @@ export const loginAPI = `${host}/api/auth/login`;
 export const addTransaction = `${host}/api/v1/addTransaction`;
 export const getTransactions = `${host}/api/v1/getTransaction`;
 export const editTransactions = `${host}/api/v1/updateTransaction`;
-=======
-// const host = "https://expense-tracker-app-knl1.onrender.com";
-const host = "https://exquisite-pavlova-ec673d.netlify.app"
-//const host = "http://localhost:5000";
-export const setAvatarAPI = `${host}/api/auth/setAvatar`;
-export const registerAPI = `${host}/api/auth/register`;
-export const loginAPI = `${host}/api/auth/login`;
-export const addTransaction = `${host}/api/v1/addTransaction`;
-export const getTransactions = `${host}/api/v1/getTransaction`;
-export const editTransactions = `${host}/api/v1/updateTransaction`;
->>>>>>> 5c92aa311c7ad603fb3e6d434f0eb57ff76e0262
 export const deleteTransactions = `${host}/api/v1/deleteTransaction`;
